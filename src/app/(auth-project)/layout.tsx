3650e7afa8d51820370ca65919ba8896526ae220
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-10-09 11:59:57 +0600

    feat: adding login page with project setup

---
+++ src/app/(auth-project)/layout.tsx
@@ -0,0 +1,15 @@
+import RightSection from "@/components/auth-layout/RightSection";
+import React from "react";
+
+export default function AuthProjectLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="grid grid-cols-2 2xl:container mx-auto min-h-screen">
+      <section>{children}</section>
+      <RightSection />
+    </main>
+  );
+}
---
+++ src/components/auth-layout/RightSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RightSection() {
+  return (
+    <div className='h-full rounded-lg p-3'>
+        <div className="bg-primary h-full rounded-lg">asdf</div>
+    </div>
+  )
+}
+
+export default RightSection
