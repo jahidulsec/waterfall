d00c227a0ff9dd10a54dafc9055458eb1717b51b
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-10-10 17:40:50 +0600

    feat: adding create new profile page

---
+++ src/app/(auth-project)/create-profile/page.tsx
@@ -0,0 +1,63 @@
+import { CircleOne, CircleTwo } from "@/components/assets/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import React from "react";
+
+function CreateProfilePage() {
+  return (
+    <section>
+      {/* header */}
+      <div className="">
+        <p className="text-sm text-muted-foreground mb-0.5">
+          Create Merger Profile
+        </p>
+        <h1 className="text-3xl font-medium">Choose your first step</h1>
+      </div>
+
+      {/* cards */}
+      <section className="my-5 flex flex-col gap-5">
+        <Card className="group hover:border-secondary/50 hover:cursor-pointer">
+          <CardContent className="flex gap-3 p-4 group-hover:bg-secondary/5">
+            <div className="logo w-[52px]">
+              <CircleOne />
+            </div>
+            <div>
+              <h2 className="text-md font-semibold mb-0.5">
+                Start from scratch
+              </h2>
+              <p className="text-xs text-muted-foreground">
+                Plan, track, and release world-class software with the #1
+                software development tool used by agile teams.
+              </p>
+              <Button variant={"link"} className="w-fit h-fit p-0 pt-3">
+                Select
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+
+        <Card className="group hover:border-secondary/50 hover:cursor-pointer">
+          <CardContent className="flex gap-3 p-4 group-hover:bg-secondary/5">
+            <div className="logo w-[52px]">
+              <CircleTwo />
+            </div>
+            <div>
+              <h2 className="text-md font-semibold mb-0.5">
+              Start with a template
+              </h2>
+              <p className="text-xs text-muted-foreground">
+                Plan, track, and release world-class software with the #1
+                software development tool used by agile teams.
+              </p>
+              <Button variant={"link"} className="w-fit h-fit p-0 pt-3">
+                Select
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </section>
+    </section>
+  );
+}
+
+export default CreateProfilePage;
---
+++ src/components/assets/card/index.ts
@@ -0,0 +1,4 @@
+import CircleOne from './CircleOne';
+import CircleTwo from './CircleTwo';
+
+export {CircleOne, CircleTwo}
